1c41825597b55a064d68a2e4c147712923202100
Aman <rajputaman2003@outlook.com>
2024-12-30 01:50:07 +0530

    solve the issue to executed only on the client side

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,42 +1,43 @@
-import React, { useRef } from 'react'
+import React, { useRef, useEffect, useState } from 'react';
 import { useInView, motion } from 'framer-motion';
-import './About.css'
+import './About.css';
 
 const About = () => {
-
-    const [mounted, setMounted] = useState(false);
-
-     // Ensure the component is rendered on the client-side
-    useEffect(
-        () => setMounted(true)
-        , []);
-
     const phrases = [
         "I'm a passionate developer with a knack for building responsive and performance-driven web applications. With experience in modern frontend technologies, I strive to deliver innovative solutions that meet user needs and exceed expectations",
-    ]
+    ];
 
     const body = useRef(null);
-    const isInView = useInView(body, { once: true, margin: "-75%" })
+    const isInView = useInView(body, { once: true, margin: "-75%" });
 
     const animation = {
         initial: { y: "100%" },
-        enter: i => ({ y: "0", transition: { duration: 0.75, ease: [0.33, 1, 0.68, 1], delay: 0.075 * i } })
+        enter: i => ({
+            y: "0",
+            transition: { duration: 0.75, ease: [0.33, 1, 0.68, 1], delay: 0.075 * i }
+        })
+    };
+
+    if (typeof window !== 'undefined') {
+        // Client-side-only code
+        return (
+            <div
+                ref={body}
+                className='body mt-52 mb-52 font-source-sans-pro'>
+                {
+                    phrases.map((phrase, index) => {
+                        return <div key={index} className='lineMask'>
+                            <motion.p custom={index} variants={animation} initial="initial" animate={isInView ? "enter" : ""}>
+                                {phrase}
+                            </motion.p>
+                        </div>
+                    })
+                }
+            </div>
+        );
     }
 
-    if (!mounted) return null;
-    return (
-        <div
-            ref={body}
-            className='body mt-52 mb-52 font-source-sans-pro '>
-            {
-                phrases.map((phrase, index) => {
-                    return <div key={index} className='lineMask'>
-                        <motion.p custom={index} variants={animation} initial="initial" animate={isInView ? "enter" : ""}>{phrase}</motion.p>
-                    </div>
-                })
-            }
-        </div>
-    )
-}
+    return null; // Return null if not in client-side rendering
+};
 
-export default About
+export default About;
